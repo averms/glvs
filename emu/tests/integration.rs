92bf0d4c73a58ac4af7ddf329a7427c000a228e2
Aman Verma <amanraoverma@gmail.com>
2025-05-01 15:30:48 -0500

    emu: fix donkey kong test.

--- emu/tests/integration.rs
+++ emu/tests/integration.rs
@@ -4,7 +4,7 @@ use emu::{Cpu, NesBus};
 
 #[test]
 fn load_donkey_kong() {
-    let f = fs::read("./resources/Donkey Kong (World) (Rev 1).nes").unwrap();
+    let f = fs::read("../resources/dk.nes").unwrap();
     let mut bus = NesBus::new(&f).unwrap();
     let _cpu = Cpu::new(&mut bus);
 }
