67ed3a6b854f10f1c3eee507ec32f85e62a75f86
Aman Verma <amanraoverma@gmail.com>
2025-05-04 17:43:53 -0500

    core: use nestest.nes for integration test and check it in.

--- glvs-core/tests/integration.rs
+++ glvs-core/tests/integration.rs
@@ -3,8 +3,8 @@ use std::fs;
 use glvs_core::{Cpu, NesBus};
 
 #[test]
-fn load_donkey_kong() {
-    let f = fs::read("../resources/dk.nes").unwrap();
+fn load_rom() {
+    let f = fs::read("./tests/nestest.nes").unwrap();
     let mut bus = NesBus::new(f.into_boxed_slice()).unwrap();
     let _cpu = Cpu::new(&mut bus);
 }
